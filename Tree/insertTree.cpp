1d48e3178a635a0e3bf7c7452a134e742353b03f
tmohammad78 <mohammadtaheri506@gmail.com>
2021-07-21 01:09:10 +0430

    Add insert tree

---
+++ Tree/insertTree.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+void inorder(Node* temp)
+{
+    if (temp == NULL)
+        return;
+
+    inorder(temp->left);
+    cout << temp->data << ' ';
+    inorder(temp->right);
+}
+Node* createNode(int data){
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->right=newNode->left = NULL;
+    return newNode;
+}
+Node* insertNode(Node* root , int data){
+    if (root == NULL) {
+        root = createNode(data);
+        return root;
+    }
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        if(temp->left != NULL){
+            q.push(temp->left);
+        }else {
+            temp->left = createNode(data);
+            return root;
+        }
+
+        if(temp->right != NULL){
+            q.push(temp->right);
+        }else{
+            temp->right = createNode(data);
+            return root;
+        }
+    }
+}
+
+int main(){
+    Node* root = createNode(10);
+    root->left = createNode(11);
+    root->left->left = createNode(7);
+    root->right = createNode(9);
+    root->right->right = createNode(8);
+    root->right->left = createNode(15);
+    inorder(root);
+    return 0;
+}
