1b1afa25a17c18c7afa2eae815f7d4849d4eef7c
tmohammad78 <mohammadtaheri506@gmail.com>
2021-08-03 01:15:33 +0430

    Delete extra function , fix issue

--- Math/primeFactor.cpp
+++ Math/primeFactor.cpp
@@ -2,41 +2,34 @@
 #include "iostream"
 
 using namespace std;
-bool isPrime(long int n)
-{
+bool isPrime(long long int n){
     if (n <= 1)
         return false;
-    for (int i = 2; i < n; i++)
+    for (long long int i = 2; i < sqrt(n); i++)
         if (n % i == 0)
             return false;
-
     return true;
 }
-void checkNum(queue<long int>& arr,queue<long int>& prime){
-    while (!arr.empty()){
-        int num = arr.front();
-       if(isPrime(num))
-           prime.push(num);
-        arr.pop();
-    }
-}
-void showQueue(queue<long int>& q){
+void showQueue(queue<long long int>& q){
     while (!q.empty()){
         int num = q.front();
         cout << num << " ";
         q.pop();
     }
 }
-void divisor(long int number,queue<long int>& arr){
-    for(int i=1 ; i<= number ; i++)
-        if(number % i == 0)
-            arr.push(i);
+void divisor(long long int number,queue<long long int>& arr){
+    for(long long int i=1 ; i<= number ; i++){
+        if(number % i == 0) {
+            if(isPrime(i))
+                arr.push(i);
+        }
+    }
+
+
 }
 int main(){
-    queue<long int> arr;
-    queue<long int> prime;
+    queue<long long int> arr;
     divisor(600851475143,arr);
-    checkNum(arr,prime);
-    showQueue(prime);
+    showQueue(arr);
     return 0;
 }
