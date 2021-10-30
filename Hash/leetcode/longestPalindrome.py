670281ba794159d8027308b7e2fbdec19dfb9e78
tmohammad78 <mohammadtaheri506@gmail.com>
2021-10-30 12:04:08 +0330

    Add longest palindrome question

---
+++ Hash/leetcode/longestPalindrome.py
@@ -0,0 +1,30 @@
+"""
+Given a string s which consists of lowercase or uppercase letters, 
+return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+"""
+
+class Solution(object):
+    def longestPalindrome(self,s):
+        ss = set()
+        for letter in s:
+            if letter not in ss:
+                ss.add(letter)
+            else:
+                ss.remove(letter)
+        if len(ss) != 0:
+            return len(s) - len(ss) + 1
+        else:
+            return len(s)
+
+
+## better solution is 
+class Solution:
+    def longestPalindrome(self, s):
+        ans = 0
+        for v in collections.Counter(s).itervalues():
+            ans += v / 2 * 2
+            if ans % 2 == 0 and v % 2 == 1:
+                ans += 1
+        return ans
